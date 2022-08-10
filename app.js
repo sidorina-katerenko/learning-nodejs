35dfff402af77971bf7bd579a2b9562a33ffc79d
kate <ksidorenko@3knbh.com>
2022-08-10 18:00:22 +0100

    base routing

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
 	console.log("Server request");
-	console.log(req.url, req.method);
 
-	res.setHeader("Content-Type", "application/json");
+	res.setHeader("Content-Type", "text/html");
 
-	// res.write('<head><link rel="stylesheet" href="#"></head>');
+	const createPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
 
-	// res.write("<h1>Hello world!</h1>");
-	// res.write("<p>My name is Kate</p>");
+	let basePath = "";
 
-	const data = JSON.stringify([
-		{
-			name: "Tommy",
-			age: 35,
-		},
-		{
-			name: "Arthur",
-			age: 40,
-		},
-	]);
+	switch (req.url) {
+		case "/":
+		case "/home":
+		case "/index.html":
+			basePath = createPath("index");
+			res.statusCode = 200;
+			break;
+		case "/about-us":
+			res.statusCode = 301;
+			res.setHeader("Location", "/contacts");
+			res.end();
+			break;
+		case "/contacts":
+			basePath = createPath("contacts");
+			res.statusCode = 200;
+			break;
+		default:
+			basePath = createPath("error");
+			res.statusCode = 404;
+			break;
+	}
 
-	res.end(data);
+	fs.readFile(basePath, (err, data) => {
+		if (err) {
+			console.log(err);
+			res.statusCode = 500;
+			res.end();
+		} else {
+			res.write(data);
+			res.end();
+		}
+	});
 });
 
 server.listen(PORT, "localhost", (error) => {
-	error ? console.log(error) : console.log("Listening to port 3000");
+	error ? console.log(error) : console.log(`Listening to port ${PORT}`);
 });
